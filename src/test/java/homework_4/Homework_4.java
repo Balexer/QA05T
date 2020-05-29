85bca525c450e528d959e951fe393429d698e592
Balexer <balexer@mail.ru>
2020-05-29 19:29:07 +0300

    DZ4

--- src/test/java/homework_4/Homework_4.java
+++ src/test/java/homework_4/Homework_4.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 public class Homework_4 {
     @Test
     public void test1() {
+
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Desktop\\dz\\TestSelenium\\src\\drivers\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://healthunify.com/bmicalculator/");
